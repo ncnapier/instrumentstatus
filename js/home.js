5c75a53c77e1c481bd656fa865373bac95de90ea
Nathaniel C Napier <nathanielclarknapier@gmail.com>
2022-06-09 16:10:39 -0500

    Add files via upload

--- js/home.js
+++ js/home.js
@@ -1,7 +1,16 @@
 document.getElementById('submit').addEventListener('click', formFillB)
 
+//date and time for chat
+let currentDate = new Date();
+// let cDay = currentDate.getDate();
+// let cMonth = currentDate.getMonth() + 1;
+// let cYear = currentDate.getFullYeaar();
+// let dateToPrint = cDay + '/' + cMonth + '/' + cYear;
 
 function formFillB() {
+    yyy = document.getElementById('statD').value 
+    
     xxx = document.getElementById('chats').value 
-    document.getElementById('chatsE').innerHTML = xxx
+    document.getElementById('chatsE').innerHTML = yyy + ":" + "<br>" + xxx + "<br>" + "(" + currentDate + ")"
+    
 }
