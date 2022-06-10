b32b25f28970f11f3896d99542a84853f428baf4
Nathaniel C Napier <nathanielclarknapier@gmail.com>
2022-06-10 11:01:46 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -143,3 +143,40 @@ function defGreen() {
 defGreen()
 
 //dmpk letter colors
+
+function letterColor() {
+    document.getElementById('D').style.color = "lightgreen"
+    document.getElementById('M').style.color = "salmon"
+    document.getElementById('P').style.color = "skyblue"
+    document.getElementById('K').style.color = "#FFFF8F"
+}
+function letterColor2() {
+    document.getElementById('M').style.color = "lightgreen"
+    document.getElementById('P').style.color = "salmon"
+    document.getElementById('K').style.color = "skyblue"
+    document.getElementById('D').style.color = "#FFFF8F"
+}
+function letterColor3() {
+    document.getElementById('P').style.color = "lightgreen"
+    document.getElementById('K').style.color = "salmon"
+    document.getElementById('D').style.color = "skyblue"
+    document.getElementById('M').style.color = "#FFFF8F"
+}
+function letterColor4() {
+    document.getElementById('K').style.color = "lightgreen"
+    document.getElementById('D').style.color = "salmon"
+    document.getElementById('M').style.color = "skyblue"
+    document.getElementById('P').style.color = "#FFFF8F"
+    
+}
+
+function flashes() {
+    x = 2;
+letterColor()
+setTimeout(letterColor2, 500)
+setTimeout(letterColor3,1000)
+setTimeout(letterColor4, 1500)
+setTimeout(flashes, x*1000);
+}
+flashes();
+//setInterval(flashes(), 2000)
