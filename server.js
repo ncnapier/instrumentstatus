d324711929f39a1af16664fe9b1caf9f6ee916d6
Nathaniel C Napier <nattydevs@gmail.com>
2022-07-21 10:03:12 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -2,18 +2,20 @@ console.log('node test')
 const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
-
+app.get('/home.css', function(req, res) {
+    res.sendFile(__dirname + "/" + "home.css");
+  });
 app.use(bodyParser.urlencoded({ extended: true}))
-
+//const PORT = 8000
 const PORT = `https://ncnapier.github.io/instrumentstatus/`
 //tells the server to listen to requests on port 8000
-app.listen(PORT, function(){
+app.listen(process.env.PORT || PORT, function(){
     console.log(`listening on port ${PORT}`)
 })
 //sends index.html when localhost:8000/ is requested by the browser
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-})
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + '/index.html')
+// })
 
 
 const MongoClient = require('mongodb').MongoClient
@@ -24,7 +26,7 @@ MongoClient.connect(`mongodb+srv://nattydevs:%2321Reipan@cluster0.u4c49.mongodb.
         const dataCollection = db.collection('quotes')
         
         //adds whatever is typed into form to the mongodb 
-        app.post('/quotes', (req, res) => {
+        app.post('/chat', (req, res) => {
             dataCollection.insertOne(req.body)
             .then(result => {
                 res.redirect('/')
@@ -35,6 +37,8 @@ MongoClient.connect(`mongodb+srv://nattydevs:%2321Reipan@cluster0.u4c49.mongodb.
             db.collection('quotes').find().toArray()
               .then(results => {
                 console.log(results)
+                res.render('index.ejs', {quotes: results})
+                
               })
               .catch(error => console.error(error))
             // ...
