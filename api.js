8a762122aba16e1f7b37f3a6777c47c9e6ad9fe3
Nathaniel C Napier <nattydevs@gmail.com>
2022-07-23 17:47:28 -0500

    Update api.js

--- api.js
+++ api.js
@@ -65,20 +65,20 @@ MongoClient.connect(`mongodb+srv://nattydevs:%2321Reipan@cluster0.u4c49.mongodb.
             
             .catch(error => console.error(error))
         })
-//         app.get('https://ncnapier.github.io/instrumentstatus/chat', (req, res) => {
-//             dataCollection.insertOne(req.body)
-//             db.collection('quotes').find().toArray()
-//             .then(results => {
-//                 console.log(results)
+        app.get('https://ncnapier.github.io/instrumentstatus/chat', (req, res) => {
+            dataCollection.insertOne(req.body)
+            db.collection('quotes').find().toArray()
+            .then(results => {
+                console.log(results)
                 
-//             })
-//             .then(result => {
-//                 res.redirect('https://ncnapier.github.io/instrumentstatus/index.html')
-//                 res.json('quotes')
-//             })
+            })
+            .then(result => {
+                res.redirect('https://ncnapier.github.io/instrumentstatus/index.html')
+                res.json('quotes')
+            })
             
             
-//         })
+        })
         app.get('https://ncnapier.github.io/instrumentstatus/index.html', (req, res) => {
             db.collection('quotes').find().toArray()
             .then(results => {
