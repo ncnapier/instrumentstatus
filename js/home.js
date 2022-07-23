11ad14387dae8bc887a9a8a1b7364b213c78c890
Nathaniel C Napier <nattydevs@gmail.com>
2022-07-23 17:08:07 -0500

    Update home.js

--- js/home.js
+++ js/home.js
@@ -96,7 +96,7 @@ fetch("https://dataservice.accuweather.com/currentconditions/v1/335668?apikey=Wl
     console.log(`error ${err}`)
 });
 
-fetch(`https://dmpk-instrument-api.herokuapp.com/api.js`)
+fetch(`https://ncnapier.github.io/instrumentstatus/api.js`)
 //fetch('https://ncnapier.github.io/instrumentstatus/api.js')
 .then(res => res.json()) // parse response as JSON
 .then(results =>{
